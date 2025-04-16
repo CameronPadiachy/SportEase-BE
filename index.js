5a1d0fed425559ed108675d4ecca037bf3891336
CameronPadiachy <cameronpadiachy@icloud.com>
2025-04-16 19:30:31 +0200

    Fixed index.js to get BE deployed

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
-    console.log('Server is runnin on por ${port}');
+    console.log(`Server is running on port ${port}`);
 });
+
