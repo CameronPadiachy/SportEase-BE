0dd72e7e5daa3bd2c880251ea6f5c3c879ae5f58
CameronPadiachy <cameronpadiachy@icloud.com>
2025-05-04 16:45:34 +0200

    changing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 /** @type {import('jest').Config} */
 module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'], // Only match files like *.test.js or *.spec.js
-    testPathIgnorePatterns: [
-      '/node_modules/',
-      '/testApp\\.js$' // Prevent testApp.js from being treated as a test
-    ],
-  };
-  
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/testApp\\.js$'
+  ],
+  // Add these new configurations:
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  moduleFileExtensions: ['js', 'json'],
+  moduleNameMapper: {
+    '^@controllers/(.*)$': '<rootDir>/controllers/$1',
+    '^@routes/(.*)$': '<rootDir>/routes/$1'
+  },
+  roots: ['<rootDir>'],
+  // For better debugging
+  verbose: true,
+  displayName: {
+    name: 'API',
+    color: 'blue'
+  }
+};
